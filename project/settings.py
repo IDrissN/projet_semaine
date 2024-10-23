a355f9e029c54e88ef96c20d994932a9fac5aa67
SSlhx <sofiane.salih.eca@gmail.com>
2024-10-23 16:13:49 +0200

    zizi

--- project/settings.py
+++ project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'semainedb',   
+        'USER': 'root',                
+        'PASSWORD': 'strong_root_password',             
+        'HOST': 'localhost',   
+        'PORT': '8081'
     }
 }
 
